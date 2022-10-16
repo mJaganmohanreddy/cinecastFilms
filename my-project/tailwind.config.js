1a2612a1057031dc36ad838614bb659b5e2c76c2
Jaganmohan <jaganre8179@gmail.com>
2022-10-16 06:05:33 +0530

    done project

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,7 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 2500ms linear',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': {
+            'opacity': '0',
+          },
+          '50%': {
+            'opacity': '0',
+          },
+          '100%': {
+            'opacity': '1',
+          }
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
